fcb71dc5ec44b83022eca5419b6c4178cb0b39ae
Omar Chouman <omar.chouman0@gmail.com>
2021-06-02 12:45:19 +0300

    Creating Table

--- answers.sh
+++ answers.sh
@@ -9,4 +9,34 @@ VALUES ("Omar", 18, "M", 300);
 6) UPDATE students SET points=(points+20) WHERE name="Basma";
 7) UPDATE students SET points=(points-10) WHERE name="Alex";
 
+CREATING TABLE:
+
+CREATE TABLE "graduates" (
+"ID" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+"Name" TEXT NOT Null UNIQUE,
+"Age" INTEGER,
+"Gender" TEXT,
+"Points" INTEGER,
+"Graduation" TEXT
+);
+
+
+COPYING LAYAL DATA:
+
+INSERT INTO  graduates (Id,Name,Age,Gender,Points)
+SELECT Id,Name,Age,Gender,Points FROM students
+WHERE name="Layal";
+
+ADD GRADUATION DATE:
+
+UPDATE graduates
+SET Graduation = '08/09/2018'
+WHERE name="Layal";
+
+DELETE THE RECORD:
+
+DELETE FROM students WHERE name = "Layal";
+
+
+
 
