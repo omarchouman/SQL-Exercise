8666ee681f522e29b325972611cba54abcc033bc
Omar Chouman <omar.chouman0@gmail.com>
2021-06-02 12:37:48 +0300

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,12 @@
+BASIC QUERIES:
+
+1) SELECT Name FROM students;
+2) SELECT * FROM students WHERE Age > 30;
+3) SELECT * FROM students WHERE Age > 30 AND Gender = "F";
+4) SELECT Points FROM students WHERE Name = "Alex";
+5) INSERT INTO students (Name, Age, Gender, Points) 
+VALUES ("Omar", 18, "M", 300); 
+6) UPDATE students SET points=(points+20) WHERE name="Basma";
+7) UPDATE students SET points=(points-10) WHERE name="Alex";
+
+
